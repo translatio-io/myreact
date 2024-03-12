715eea04347509dd0d17ee29e0b2df3853c6a0f3
Yu Shao <yu.shao.au@gmail.com>
2024-03-12 14:08:35 -0400

    mar122024

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_URLS = {
   host: 'https://api2.translatio.io',
-  authUrl: "http://auth.translatio.io",
+  authUrl: "https://auth.translatio.io",
   authRealm: "translatio",
   authClientId: "translatio",
 };
